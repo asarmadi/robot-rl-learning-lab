ca6dce229e091e8a42db92da6550a531eed5e2f0
asarmadi <a.sarmadi@yahoo.com>
2026-07-04 13:04:25 -0400

    max action added

--- agents/epsilon_greedy.py
+++ agents/epsilon_greedy.py
@@ -2,7 +2,7 @@ import numpy as np
 from agents.agent import Agent
 
 class epsilonGreedy(Agent):
-    def __init__(self, action_dim, seed, epsilon_max, epsilon_min=0.05, decay_rate=0.00001, environment='GridWorld'):
+    def __init__(self, action_dim, seed, epsilon_max, max_action=2, epsilon_min=0.05, decay_rate=0.00001, environment='GridWorld'):
         super().__init__(seed=seed, action_dim=action_dim)
         self.epsilon_max = epsilon_max
         self.epsilon_min = epsilon_min
@@ -12,7 +12,7 @@ class epsilonGreedy(Agent):
         if environment == 'GridWorld':
             self.actions = [np.array([-1,0]), np.array([1,0]), np.array([0,1]), np.array([0,-1])] # Down, Up, Right, Left
         else:
-            self.actions = np.linspace(-10,10,action_dim)
+            self.actions = np.linspace(-max_action,max_action,action_dim)
 
     def act(self, action_value):
         if np.random.random() < self.epsilon:

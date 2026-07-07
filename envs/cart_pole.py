threshold calculation bug fixed

envs/cart_pole.py
@@ -13,7 +13,7 @@ class CartPole(Environment):
 
         # We define the state as position, velocity, rotation, angular velocity x,x_d,\theta, \theta_d
         self.init_state     = torch.zeros((self.state_dim,1))
-        self.init_state[2]  = torch.pi//18
+        self.init_state[2]  = torch.pi/18
         self.terminal_state = torch.zeros((self.state_dim,1))
         # Cart-pole hyper-parameters
         self.M               = 1.0
@@ -22,13 +22,10 @@ class CartPole(Environment):
         self.g               = 9.8
         self.dt              = 0.02 # 50 Hz
         self.terminate       = 'running'
-        self.reach_threshold = 2*(torch.pi//180) # 2 Degree boundary
+        self.reach_threshold = 2*(torch.pi/180) # 2 Degree boundary
         self.x_lim           = 2   # We are limiting the x to be between a thershold to prevent going to infinity
         self.upright_counter = 0   # TO keep track of the agent when it is stable upright
-        self.upright_threshold = 50 # Number of steps that we consider the pole to be upright
-
-        if 'upright' in method:
-            self.upright_threshold = 50
+        self.upright_threshold = 200 # Number of steps that we consider the pole to be upright
 
 
         self.save_dir        = f'./logs/cartPole_{method}'
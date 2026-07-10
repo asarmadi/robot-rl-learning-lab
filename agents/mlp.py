cb37b82e7672869b7f4b90141d3bb0b0b5531208
Alireza <a.sarmadi@yahoo.com>
2026-07-10 13:15:03 -0400

    softmax removed

--- agents/mlp.py
+++ agents/mlp.py
@@ -7,7 +7,6 @@ class MLP(nn.Module):
 
         self.layers     = nn.ModuleList()
         self.activation = nn.ReLU()
-        self.softmax    = nn.Softmax(dim=0)
 
         self.layers.append(nn.Linear(input_dim, hidden_dim))
 
@@ -37,8 +36,7 @@ class MLP(nn.Module):
             # Not applying the activation function to the last layer
             if i < len(self.layers) - 1:
                 x = self.activation(x)
-        # To get the probabilities for the last layer
-        #x = self.softmax(x)
+
         return x
     
     def predict(self, x):

58dd859f3becdab7e38daa31d1e8594130726362
Alireza <a.sarmadi@yahoo.com>
2026-07-11 16:25:04 -0400

    continuous action added

---
+++ agents/mlp_continuous.py
@@ -0,0 +1,47 @@
+import torch
+import torch.nn as nn
+
+class MLPC(nn.Module):
+    def __init__(self, input_dim, hidden_dim, n_layers, output_dim, max_action=10):
+        super().__init__()
+
+        self.layers     = nn.ModuleList()
+        self.activation = nn.ReLU()
+
+        self.layers.append(nn.Linear(input_dim, hidden_dim))
+
+        for i in range(n_layers):
+            self.layers.append(nn.Linear(hidden_dim, hidden_dim))
+
+        self.layers.append(nn.Linear(hidden_dim, output_dim*2)) # For each output, we calculate mean and std
+
+        self.init_weights()
+
+    def init_weights(self):
+        # Normal hidden-layer initialization
+        for layer in self.layers:
+            if isinstance(layer, nn.Linear):
+                nn.init.xavier_uniform_(layer.weight)
+                nn.init.zeros_(layer.bias)
+
+        # Make initial logits exactly zero
+        nn.init.zeros_(self.layers[-1].weight)
+        nn.init.zeros_(self.layers[-1].bias)
+
+    def forward(self, x):
+        for i, layer in enumerate(self.layers):
+            x = layer(x)
+            # Not applying the activation function to the last layer
+            if i < len(self.layers) - 1:
+                x = self.activation(x)
+
+        return x
+    
+    def predict(self, x):
+        device = next(self.parameters()).device
+        x = torch.as_tensor(x, dtype=torch.float32, device=device)
+
+        with torch.no_grad():
+            return self(x)
+
+
